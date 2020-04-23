95d23676f22c4c51eecb50c1b01e29c4388e2a03
gutcod <gutsanomania@gmail.com>
2020-04-23 23:15:56 +0200

    Updates

---
+++ precache-manifest.07a69e57706d563d8de058a2e33db745.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b727a5e08e734671cdb6cab4db460965",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "17066199fbc8ee3fc9f1",
+    "url": "/robofriend/static/css/2.2dc38268.chunk.css"
+  },
+  {
+    "revision": "8feda0916384569e8dd6",
+    "url": "/robofriend/static/css/main.2f2910d9.chunk.css"
+  },
+  {
+    "revision": "17066199fbc8ee3fc9f1",
+    "url": "/robofriend/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8feda0916384569e8dd6",
+    "url": "/robofriend/static/js/main.15afec17.chunk.js"
+  },
+  {
+    "revision": "78b7baca1a7d808500f9",
+    "url": "/robofriend/static/js/runtime-main.d5f1d0e3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
